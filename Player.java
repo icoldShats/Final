05c9a26ff5489c51ba216f4ae71f90e42a4a2dba
Medina Sagudenova <medina.sagudenova@narxoz.kz>
2025-04-21 19:20:15 +0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,13 +1,3 @@
-public class Player {
-
-    private String name;
-    private int health;
-    private int age;
-
-    public Player(String name, int age,int health){
-        this.name = name;
-        this.age = age;
-        this.health = health;
-    }
-
-}
+import state.PlayerState;
+import state.SmallMario;
+import state.BigMario;
