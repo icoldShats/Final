f4c461f13f99aa13ad3e90a41e537d2d6ad74581
icoldShats <medina.sagudenova@narxoz.kz>
2025-05-07 18:45:14 +0600

    work with roratingBlade

--- Desktop/patternfinalprj (1)/patternfinalprj/src/RotatingBlade.java
+++ Desktop/patternfinalprj (1)/patternfinalprj/src/RotatingBlade.java
@@ -6,4 +6,7 @@ import java.awt.geom.AffineTransform;
 public class RotatingBlade extends GameObject {
     private int angle = 0;
 
+    public RotatingBlade(int x, int y, int size) {
+        super(x, y, size, size);
+    }
 }
