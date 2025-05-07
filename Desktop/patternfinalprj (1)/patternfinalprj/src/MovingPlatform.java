a02833eceb4878398a7bf23f193392ee2a49c13c
icoldShats <medina.sagudenova@narxoz.kz>
2025-05-07 18:58:27 +0600

    start work movingPlatform

--- Desktop/patternfinalprj (1)/patternfinalprj/src/MovingPlatform.java
+++ Desktop/patternfinalprj (1)/patternfinalprj/src/MovingPlatform.java
@@ -5,4 +5,11 @@ import java.awt.*;
 public class MovingPlatform extends GameObject {
     private int speed;
     private int leftBound, rightBound;
+
+    public MovingPlatform(int x, int y, int width, int height, int speed, int leftBound, int rightBound) {
+        super(x, y, width, height);
+        this.speed = speed;
+        this.leftBound = leftBound;
+        this.rightBound = rightBound;
+    }
 }
