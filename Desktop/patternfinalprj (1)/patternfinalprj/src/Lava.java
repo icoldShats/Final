fd011fa33bae21ac6c3695379b9812a7cfaeab66
icoldShats <medina.sagudenova@narxoz.kz>
2025-05-07 19:08:08 +0600

    start work Lava

--- Desktop/patternfinalprj (1)/patternfinalprj/src/Lava.java
+++ Desktop/patternfinalprj (1)/patternfinalprj/src/Lava.java
@@ -6,4 +6,8 @@ public class Lava extends GameObject{
     public Lava(int x, int y, int width, int height) {
         super(x, y, width, height);
     }
+
+    @Override
+    public void update() {
+    }
 }
